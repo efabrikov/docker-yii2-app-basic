3ffd1e9b49d79619b023e8ca02196fd3dc9de674
Eugene Fabrikov <eugene.fabrikov@gmail.com>
2017-09-28 14:51:24 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,16 +18,16 @@ for _curVar in `env | awk -F = '{print $1}'`;do
 done
 
 # prepare log output
-mkdir -p /app/runtime/logs /app/web/assets
+#mkdir -p /app/runtime/logs /app/web/assets
 touch /var/log/nginx/access.log \
       /var/log/nginx/error.log \
-      /app/runtime/logs/web.log \
-      /app/runtime/logs/console.log
+#      /app/runtime/logs/web.log \
+#      /app/runtime/logs/console.log
 # adjust folder permissions for docker volume usage
-find /app/runtime -type d -print0 | xargs -0 chmod 777
-find /app/runtime -type f -print0 | xargs -0 chmod 666
-find /app/web/assets -type d -print0 | xargs -0 chmod 777
-find /app/web/assets -type f -print0 | xargs -0 chmod 666
+#find /app/runtime -type d -print0 | xargs -0 chmod 777
+#find /app/runtime -type f -print0 | xargs -0 chmod 666
+#find /app/web/assets -type d -print0 | xargs -0 chmod 777
+#find /app/web/assets -type f -print0 | xargs -0 chmod 666
 
 # start PHP and nginx
 service php5-fpm start
